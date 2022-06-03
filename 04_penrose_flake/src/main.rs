483a83e7c385db8add72bb92630e6b5268251be8
danieligazit <danieligazit@gmail.com>
2022-06-04 01:37:23 +0300

    Penrose working

--- 04_penrose_flake/src/main.rs
+++ 04_penrose_flake/src/main.rs
@@ -6,33 +6,64 @@ mod penrose_flake;
 
 fn main() {
     nannou::app(model)
-        // .loop_mode(LoopMode::loop_once())
+        .loop_mode(LoopMode::rate_fps(64.0))
         .update(update)
         .run();
 }
 
 struct Model {
-    flake: penrose_flake::PenroseFlake
+    flakes: Vec<penrose_flake::PenroseFlake>
 }
 
 fn model(app: &App) -> Model {
     app.new_window()
         .view(view)
         .build()
-        .unwrap();
+        .unwrap();  
 
-    Model {flake: penrose_flake::PenroseFlake::new()}
+
+    Model {flakes: Vec::new()}
 }
 
 
 fn update(app: &App, model: &mut Model, _update: Update) {
-    // model.flake.update();
+    for flake in model.flakes.iter_mut() {
+        flake.update();
+    }
+
+    for (frame, pos, axiom, rule, color, theta) in [
+        // (0, app.window_rect().top_left(), WHITE)       
+        (0, vec2(-300.0, 20.0), "F3-F3-F3-F3-F", "F3-F3-F45-F++F3-F", WHITE, PI / 10.0),
+        // (30, app.window_rect().top_left(), "F", "F+F-F", LIGHTGRAY, PI * 2.0/ 3.0),
+        (0, vec2(50.0, 0.0), "F", "F+F-F", LIGHTGRAY, PI * 2.0/ 3.0),
+        (0, vec2(0.0, -200.0), "F", "+F+F-F-F+F+F+F-F-F+F", CYAN, PI / 2.0),
+
+    ]{
+        if app.elapsed_frames() != frame {continue;}
+        println!("{}", app.elapsed_frames());
+
+        let mut flake = penrose_flake::PenroseFlake::new(
+            pos,
+            color,
+            axiom,
+            rule,
+            theta,
+        );
+
+
+        flake.reset();
+        flake.simulate(4);
+        model.flakes.push(flake);
+    }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    draw.background().color(WHITE);
+    // draw.background().color(WHITE);
 
-    model.flake.view(draw);
+    for flake in model.flakes.iter() {
+        flake.view(&draw);
+    }
+
     draw.to_frame(app, &frame).unwrap();
 }
--- 04_penrose_flake/src/penrose_flake.rs
+++ 04_penrose_flake/src/penrose_flake.rs
@@ -1,85 +1,162 @@
 use nannou::prelude::*;
-
+use palette::rgb::Srgb;
+use std::cmp;
 pub struct PenroseFlake {
-    pub axiom: &'static str,
-    pub rule_f: &'static str,
+    pub axiom: String,
+    pub rule_f: String,
     pub start_length: f32,
     pub draw_length: f32,
-    pub rotation: f32,
     pub generations: u64,
     pub steps: usize,
-    pub production: &'static str,
-    pos: Vec2,
+    pub repeats: u32,
+    pub step: u32,
+    pub production: String,
+    pub buf: Vec<Vec2>,
+    pub pos: Vec2,
+    pub theta: f32,
+    pub base_theta: f32,
+    pub color: Srgb<u8>,
 }
 
 const THETA: f32 = PI / 10.0;
 
 impl PenroseFlake {
-    pub fn new() -> PenroseFlake {
+    pub fn new(pos: Vec2, color: Srgb<u8>, axiom: &str, rule: &str, base_theta: f32) -> PenroseFlake {
         PenroseFlake {
-            axiom: "F3-F3-F3-F3-F",
-            rule_f: "F3-F3-F45-F++F3-F",
-            start_length: 450.0,
+            axiom: axiom.into(),
+            rule_f: rule.into(),
+            start_length: 500.0,
             draw_length: 0.0,
-            rotation: THETA,
             generations: 0,
             steps: 0,
-            production: "",
-            pos: pt2(0.0, 0.0)
+            production: "".into(),
+            buf: Vec::new(),
+            pos: pos,
+            repeats: 1,
+            step: 0,
+            theta: 0.0,
+            base_theta: base_theta,
+            color: color,
         }
-    
+        
+    }
+
+    pub fn simulate(&mut self, gen: u64){
+        while self.generations < gen {
+            self.production = self.production.replace("F", &self.rule_f);
+            self.draw_length *= 0.4;
+            self.generations += 1;
+        }
     }
 
     pub fn reset(&mut self) {
-        self.production = self.axiom;
+        self.steps = cmp::max(3, self.production.len());
+        self.production = (&self.axiom).into();
         self.draw_length = self.start_length;
         self.generations = 0;
+
     }
 
-    pub fn view(&mut self, draw: &Draw) {
-        //translate(width, height)
-        let mut repeats = 1;
-        self.steps += 3;
+    pub fn update(&mut self) {
+        self.pos = match self.buf.last(){
+            None => self.pos,
+            Some(pos) => *pos
+        };
 
-        if self.steps > self.production.len() {
-            self.steps = self.production.len();
+        self.buf.clear();
+
+        self.step += 1;
+
+        if self.step >= self.production.len().try_into().unwrap() {
+            self.step = 0;
+            self.theta = 0.0;
         }
+        let step = self.production.chars().nth(self.step as usize).unwrap();
 
-        for i in 0..self.steps {
-            let step = self.production.chars().nth(i).unwrap();
-
-            match step {
-                'F' => {
-                    for j in 0..repeats {
-                        draw.line()
-                            .start(self.pos)
-                            ;
-                            // .end(pt2(self.pos.x, self.))
-                        
-                    
-                        // translate(0, -self.draw_length)
-                    }
-                    repeats = 1;
+        match step {
+            'F' => {
+                for _ in 0..self.repeats {
+                    let point = pt2(
+                        self.pos.x - (self.draw_length * self.theta.sin()),  
+                        self.pos.y - (self.draw_length * self.theta.cos())
+                    );
+                    self.buf.push(point);
                 }
-                '+' => {
-                    for j in 0..repeats {
-                        self.rotation += THETA;    
-                    }
-                    repeats = 1;
-                }
-                '-' => {
-                    for j in 0..repeats {
-                        self.rotation -= THETA;    
-                    }
-                    repeats = 1; 
-                }
-                _ => {}
+                self.repeats = 1;
             }
+            '+' => {
+                for _ in 0..self.repeats {
+                    self.theta += self.base_theta;    
+                }
+                self.repeats = 1;
+            }
+            '-' => {
+                for _ in 0..self.repeats {
+                    self.theta -= self.base_theta;    
+                }
+                self.repeats = 1; 
+            }
+            '0'..='9' => {
+                self.repeats += step as u32 - 48;
+            }
+            _ => {}
         }
+
+        
+        // for _ in 0..times {
+        //     self.update_once();
+        // }
         
     }
 
-    pub fn update() {
-        
+    fn update_once(&mut self) {
+        self.step += 1;
+
+        let step = self.production.chars().nth(self.step as usize).unwrap();
+
+        match step {
+            'F' => {
+                for _ in 0..self.repeats {
+                    let point = pt2(
+                        self.pos.x - (self.draw_length * self.theta.sin()),  
+                        self.pos.y - (self.draw_length * self.theta.cos())
+                    );
+                    self.buf.push(point);
+                }
+                self.repeats = 1;
+            }
+            '+' => {
+                for _ in 0..self.repeats {
+                    self.theta += THETA;    
+                }
+                self.repeats = 1;
+            }
+            '-' => {
+                for _ in 0..self.repeats {
+                    self.theta -= THETA;    
+                }
+                self.repeats = 1; 
+            }
+            '0'..='9' => {
+                self.repeats += step as u32 - 48;
+            }
+            _ => {}
+        }
     }
+
+    pub fn view(&self, draw: &Draw) {
+        let mut pos = self.pos;
+        for point in &self.buf{
+            draw.line()
+                .start(pos)
+                .end(*point)
+                .weight(1.0)
+                .color(self.color)
+                ;
+            pos = *point;
+        }
+                    
+    }
+
+   
 }
